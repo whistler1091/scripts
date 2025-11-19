296150185ebbe9486e40b94e99424197c00d5839
whistler1091 <steve.lockley@gmail.com>
2025-11-19 14:42:03 +0000

    Update and rename reboot-standby.sh to reboot.sh

--- reboot-standby.sh
+++
@@ -1,4 +0,0 @@
-bash
-# initially this script should be private and retain the "-STANDBY" before being activated by being public and removing "-STANDBY".
-sudo crontab -l 2>/dev/null | grep -qxF "20 14 * * * /sbin/shutdown -r now" \
-  || ( sudo crontab -l 2>/dev/null; echo "20 14 * * * /sbin/shutdown -r now" ) | sudo crontab -
