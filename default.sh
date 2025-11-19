b462bcd1c7c3a16a97f3a87c97c617e45ac1021e
whistler1091 <steve.lockley@gmail.com>
2025-11-19 15:41:44 +0000

    Update default.sh

--- default.sh
+++ default.sh
@@ -1,4 +1,4 @@
 bash
-# initially this script should be private and retain the "-STANDBY" before being activated by being public and removing "-STANDBY".
+#This is a test script that populates the crontab file with the below text. 
 sudo crontab -l 2>/dev/null | grep -qxF "#CRON_STANDBY" \
   || ( sudo crontab -l 2>/dev/null; echo "#CRON_STANDBY" ) | sudo crontab -
