63ff4feca36e299a941fced2e1d94f4c968d35fc
whistler1091 <steve.lockley@gmail.com>
2025-11-19 13:32:10 +0000

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,4 +1,3 @@
 bash
 sudo crontab -l 2>/dev/null | grep -qxF "37 12 * * * /sbin/shutdown -r now" \
   || ( sudo crontab -l 2>/dev/null; echo "37 12 * * * /sbin/shutdown -r now" ) | sudo crontab -
-fi
