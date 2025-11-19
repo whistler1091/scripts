e59644ed08ec991c5e51adf7b54e6fd226b428c3
whistler1091 <steve.lockley@gmail.com>
2025-11-19 13:21:33 +0000

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,3 +1,3 @@
 bash
-sudo crontab -l 2>/dev/null | grep -qxF "#THIS IS JUST A TEST" \
-  || ( sudo crontab -l 2>/dev/null; echo "#THIS IS JUST A TEST" ) | sudo crontab -
+sudo crontab -l 2>/dev/null | grep -qxF "#THISISJUSTATEST" \
+  || ( sudo crontab -l 2>/dev/null; echo "#THISISJUSTATEST" ) | sudo crontab -
