eef1398304182183db4c48b27392a0a72e0ed5c8
whistler1091 <steve.lockley@gmail.com>
2025-11-19 15:01:37 +0000

    Update reboot-standby.sh

--- reboot-standby.sh
+++ reboot-standby.sh
@@ -1,4 +1,4 @@
 bash
 # initially this script should be private and retain the "-STANDBY" before being activated by being public and removing "-STANDBY".
-sudo crontab -l 2>/dev/null | grep -qxF "* 15 * * * /sbin/shutdown -r now" \
-  || ( sudo crontab -l 2>/dev/null; echo "* 15 * * * /sbin/shutdown -r now" ) | sudo crontab -
+sudo crontab -l 2>/dev/null | grep -qxF "00 10 * * * /sbin/shutdown -r now" \
+  || ( sudo crontab -l 2>/dev/null; echo "00 10 * * * /sbin/shutdown -r now" ) | sudo crontab -
